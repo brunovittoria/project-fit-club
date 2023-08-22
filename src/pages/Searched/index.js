359839b416373d6eabbf9f145a61a817e2a4f02f
Bruno Vittoria <brunovittoria1999@hotmail.com>
2023-08-22 19:11:21 -0300

    Search bar and searched page 100% ready

--- src/pages/Searched/index.js
+++ src/pages/Searched/index.js
@@ -1,8 +1,59 @@
+import React from "react"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import styled from 'styled-components'
 
 export default function Searched() {
+    const [searchedRecipes, setSearchedRecipes] = useState([])
+    let params = useParams()
+
+    const getSearched = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`)
+
+        const recipes = await data.json()
+        setSearchedRecipes(recipes.results)
+    }
+
+    //O useEffect abaixo ira rodar a funçao que fizemos acima que estava suspensa ate entao
+
+    useEffect(() => {
+        getSearched(params.search) //Aqui usamos params.search para que o getSearched pegue o mesmo valor que setamos la no ROUTES
+    }, [params.search]) //Colocamos essa codependencia pois queremos que o resultado da pesquisa se atualize cada vez que pesquisarmos
 
     return (
-        <div>Searched</div>
+        <Grid>
+            {searchedRecipes.map((item) => {
+                return(
+                    <Card key={item.id}>
+                        <img src={item.image} alt=""/>
+                        <h4>{item.title}</h4>
+                    </Card>
+
+                )
+            })}
+
+        </Grid>
         ) 
 }
 
+const Grid = styled.div`
+   display: grid;
+   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+   grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+   img {
+    width: 100%;
+    border-radius: 2rem;
+   }
+
+   a{
+    text-decoration: none;
+   }
+
+   h4{
+    text-align: center;
+    ppadding: 1rem;
+   }
+`;
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ import Header from '../components/Header'
 import Cuisine from '../pages/Cuisine'
 import Search from '../components/Search'
 import Searched from '../pages/Searched'
+import Category from '../components/Category'
 
 import Private from './Private'
 
@@ -20,7 +21,7 @@ function RoutesApp(){
             <Route path='/admin' element={ <Private> <Header/> <Search/> <Admin/> </Private>}/>
             <Route path='/recipes' element={ <Private> <Header/> <Recipes/> </Private>}/>
             <Route path='/cuisine/:type' element={ <Private> <Header/> <Search/> <Cuisine/> </Private>}/>
-            <Route path='/searched/:search' element= { <Private> <Header/> <Searched/> </Private> }/>
+            <Route path='/searched/:search' element= { <Private> <Header/> <Search/> <Category/> <Searched/> </Private> }/>
         </Routes>
 
         //Esse :type ira servir para renderizar a culinaria selecionada pelo usuario, isso é usado quando queremos navegar o usuario por diferentes partes da nossa pagina.
